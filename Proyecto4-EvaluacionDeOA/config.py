963ef25ea36b7352f7e29a048dc419f1db6bd8ec
torrezww <242310897@itslerdo.edu.mx>
2026-06-06 15:04:03 -0600

    Add files via upload

---
+++ Proyecto4-EvaluacionDeOA/config.py
@@ -0,0 +1,23 @@
+# ==========================================
+# CONFIGURACIÓN GENERAL
+# ==========================================
+
+NUM_REGISTROS = 1000000
+
+ARCHIVO_CSV = "pacientes.csv"
+ARCHIVO_JSON = "pacientes.json"
+
+nombres = ["Ana", "Carlos", "Juan", "Maria", "Luis", "Elena", "Pedro", "Sofia"]
+
+apellidos = ["Lopez", "Ruiz", "Gomez", "Perez", "Martinez", "Hernandez", "Diaz"]
+
+medicamentos = ["Paracetamol", "Ibuprofeno", "Amoxicilina", "Omeprazol", "Metformina"]
+
+especialidades = ["Cardiologia", "Pediatria", "General", "Urgencias", "Ginecologia"]
+
+enfermedades = ["Gripe", "Diabetes", "Hipertension", "Covid", "Migraña"]
+
+meses = [
+    "Enero","Febrero","Marzo","Abril","Mayo","Junio",
+    "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
+]
